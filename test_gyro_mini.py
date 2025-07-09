f6ad373f84b452e389f88e321521971602628a16
Raphael_Buchinger21ws <k11932272@students.jku.at>
2025-07-09 11:48:32 +0200

    faster

--- test_gyro_mini.py
+++ test_gyro_mini.py
@@ -3,12 +3,14 @@ from mpu6050 import mpu6050
 import time
 
 
-DT = 500
+DT = 100
 
 
 sensor = mpu6050(0x68)
 
 next_time = time.time() + DT / 1000.0
+counter = 0
+g_arr=[[], [], []]  # Initialize arrays for gx, gy, gz
 
 while True:
 	if time.time() >= next_time:
@@ -16,9 +18,13 @@ while True:
 
 		# Read gyro data
 		current_gyro = sensor.get_gyro_data()
-		gx = current_gyro['x']
-		gy = current_gyro['y']
-		gz = current_gyro['z']
+		gx =+ current_gyro['x']
+		gy =+ current_gyro['y']
+		gz =+ current_gyro['z']
+		counter += 1
+		if counter == 10:
+			print(f"Gyro: gx={gx}, gy={gy}, gz={gz}")
+			gx, gy, gz = 0, 0, 0
+			counter = 0
 
-		print(f"Gyro: gx={gx}, gy={gy}, gz={gz}")
 
