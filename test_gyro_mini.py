e9c781a76c9c24ba9d28309038858c6c91e9968e
Raphael_Buchinger21ws <k11932272@students.jku.at>
2025-07-09 11:50:25 +0200

    accel

--- test_gyro_mini.py
+++ test_gyro_mini.py
@@ -10,21 +10,20 @@ sensor = mpu6050(0x68)
 
 next_time = time.time() + DT / 1000.0
 counter = 0
-g_arr=[[], [], []]  # Initialize arrays for gx, gy, gz
 
 while True:
 	if time.time() >= next_time:
 		next_time += DT / 1000.0
 
 		# Read gyro data
-		current_gyro = sensor.get_gyro_data()
-		gx =+ current_gyro['x']
-		gy =+ current_gyro['y']
-		gz =+ current_gyro['z']
+		current_accel = sensor.get_accel_data()
+		ax =+ current_accel['x']
+		ay =+ current_accel['y']
+		az =+ current_accel['z']
 		counter += 1
 		if counter == 10:
-			print(f"Gyro: gx={gx}, gy={gy}, gz={gz}")
-			gx, gy, gz = 0, 0, 0
+			print(f"Gyro: gx={ax}, gy={ay}, gz={az}")
+			ax, ay, az = 0, 0, 0
 			counter = 0
 
 
