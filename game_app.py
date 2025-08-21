a5aa1ce24635fb4b2e0d43be30467ee7d2706157
Raphael_Buchinger21ws <k11932272@students.jku.at>
2025-08-21 17:21:48 +0200

    modulo?

--- game_app.py
+++ game_app.py
@@ -207,7 +207,7 @@ class GameApp:
         if not self.config.online_mode:
             self.time_passed = self.get_elapsed_time()
             minutes = int(self.time_passed // 60)
-            seconds = self.time_passed % 60
+            seconds = int(self.time_passed) % 60
             self.canvas.itemconfig(self.time_text, text=f"Time: {minutes:02}:{seconds:04.1f}")
 
         self.vibro_motor.update()
